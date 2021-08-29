7b0daeb6b587ae30132286bcaa8b8120980497cb
Anna <annakozlowska24@gmail.com>
2021-08-29 20:13:09 +0200

    first commit

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,174 @@
+/// <reference types="cypress"/>
+
+//const exp = require("constants")
+//const { eq } = require("cypress/types/lodash")
+//const { findFormatter } = require("tslint")
+
+describe('Our first suite', () =>{
+
+    it('first test', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //by Tag Name
+        cy.get('input')
+
+        //by ID
+        cy.get('#inputEmail1')
+
+        //by Class name
+        cy.get('.input-full-width')
+
+        //by Attribute name
+        cy.get('[placeholder]')
+
+        //by Attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        //by Class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        // by Tag name and Attribute with value
+        cy.get('input[placeholder="Email"]')
+
+        // by two diffrent attributes
+        cy.get('[placeholder="Email"][type="email"]')
+
+         // by Tag name, Attribute with value, ID and Class name
+         cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+         //The most recommended way by Cypress
+         cy.get('[data-cy="imputEmail1"]')
+    })
+
+    it('second test', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.get('[data-cy="signInButton"]')
+
+        cy.contains('Sign in')
+        cy.contains('[status="warning"]','Sign in')
+
+        cy.get('#inputEmail3')
+            .parents('form')
+            // "find" find child elements from parents elements
+            .find('button')
+            .should('contain','Sign in')
+            .parents('form')
+            .find('nb-checkbox')
+            .click()
+
+            cy.contains('nb-card','Horizontal form').find('[type="email"]')
+    })
+
+    it('then and wrap methods', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //cy.contains('nb-card', 'Using the Grid').find('[for="inputEmail1"]').should('contain', 'Email')
+        //cy.contains('nb-card', 'Using the Grid').find('[for="inputPassword2"]').should('contain', 'Password')
+        
+        //cy.contains('nb-card', 'Basic form').find('[for="exampleInputEmail1"]').should('contain', 'Email address')
+        //cy.contains('nb-card', 'Basic form').find('[for="exampleInputPassword1"]').should('contain', 'Password')
+
+        cy.contains('nb-card', 'Using the Grid').then(firstForm => {
+            const emailLabelFirst = firstForm.find('[for="inputEmail1"]').text()
+            const passwordLabelFirst = firstForm.find('[for="inputPassword2"]').text()
+            expect(emailLabelFirst).to.equal('Email')
+            expect(passwordLabelFirst).to.equal('Password')
+
+
+            cy.contains('nb-card', 'Basic form').then(secondForm => {
+                const passwordSecondText = secondForm.find('[for="exampleInputPassword1"]').text()
+                expect(passwordLabelFirst).to.equal(passwordSecondText)
+
+                cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain', 'Password')
+            })
+        })
+    })
+
+    it('invoke command', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //2
+        cy.get('[for="exampleInputEmail1"]').then(label => {
+            expect(label.text()).to.equal('Email address')
+        })
+
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text => {
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr', 'class')
+            .should('contain', 'checked')
+    })
+
+    it('assert property', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+            cy.wrap(input).click()
+            cy.get('nb-calendar-picker').contains('17').click()
+            cy.wrap(input).invoke('prop', 'value').should('contain', 'Aug 17, 2021')
+        })
+    })
+
+    it('radio button', () => {
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then(radioButtons => {
+
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+                .should('be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force: true})
+                
+            cy.wrap(radioButtons)
+                .first()
+                .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled') 
+        })
+    })
+
+    it.only('check boxes', () => {
+
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        //cy.get('[type="checkbox"]').check({force: true})
+        cy.get('[type="checkbox"]').eq(0).click({force: true})
+        cy.get('[type="checkbox"]').eq(1).check({force: true})
+    })
+})
