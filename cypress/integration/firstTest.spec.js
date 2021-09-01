c92daddaa0b1b0e871578a583975d53764e26f24
Anna <annakozlowska24@gmail.com>
2021-09-01 13:58:51 +0200

    nowa wersja cypress, kolejne testy

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,9 +1,5 @@
 /// <reference types="cypress"/>
 
-//const exp = require("constants")
-//const { eq } = require("cypress/types/lodash")
-//const { findFormatter } = require("tslint")
-
 describe('Our first suite', () =>{
 
     it('first test', () => {
@@ -101,11 +97,16 @@ describe('Our first suite', () =>{
         cy.contains('Form Layouts').click()
 
         //1
-        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+        cy.get('[for="exampleInputEmail1"]')
+            .should('contain', 'Email address')
+            .should('have.class', 'label')
+            .and('have.text', 'Email address')
 
         //2
         cy.get('[for="exampleInputEmail1"]').then(label => {
             expect(label.text()).to.equal('Email address')
+            expect(label).to.have.class('label')
+            expect(label).to.have.text('Email address')
         })
 
         //3
@@ -161,7 +162,7 @@ describe('Our first suite', () =>{
         })
     })
 
-    it.only('check boxes', () => {
+    it('check boxes', () => {
 
         cy.visit('/')
         cy.contains('Modal & Overlays').click()
@@ -171,4 +172,151 @@ describe('Our first suite', () =>{
         cy.get('[type="checkbox"]').eq(0).click({force: true})
         cy.get('[type="checkbox"]').eq(1).check({force: true})
     })
+
+    it('Lists and dropdowns', () => {
+
+        cy.visit('/')
+
+        //1
+        //cy.get('nav nb-select').click()
+        //cy.get('.options-list').contains('Dark').click()
+        //cy.get('nav nb-select').should('contain', 'Dark')
+        //cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+        //2
+        cy.get('nav nb-select').then(dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list nb-option').each((listItem, index) => {
+               const itemText = listItem.text().trim()
+
+               const colors = {
+                   "Light": "rgb(255, 255, 255)",
+                   "Dark": "rgb(34, 43, 69)",
+                   "Cosmic": "rgb(50, 50, 89)",
+                   "Corporate": "rgb(255, 255, 255)"
+               }
+
+               cy.wrap(listItem).click()
+               cy.wrap(dropdown).should('contain', itemText)
+               cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+               if(index < 3){
+                cy.wrap(dropdown).click()
+               }
+            })
+        })
+    })
+
+    it('Web tables', () => {
+
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1
+        cy.get('tbody').contains('tr', 'Larry').then(tableRow => {
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type('25')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
+        })
+
+        //2
+        cy.get('thead').find('.nb-plus').click()
+        cy.get('thead').find('tr').eq(2).then(tableRow => {
+            cy.wrap(tableRow).find('[placeholder="First Name"]').type('Anna')
+            cy.wrap(tableRow).find('[placeholder="Last Name"]').type('Test')
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+        })
+
+        cy.get('tbody tr').first().find('td').then(tableColumns => {
+            cy.wrap(tableColumns).eq(2).should('contain', 'Anna')
+            cy.wrap(tableColumns).eq(3).should('contain', 'Test')
+        })
+
+        //3
+        const age = [20, 30, 40, 200]
+
+        cy.wrap(age).each(age => {
+            cy.get('thead [placeholder="Age"]').clear().type(age)
+            cy.wait(500)
+            cy.get('tbody tr').each(tableRow => {
+                if(age == 200){
+                    cy.wrap(tableRow).should('contain','No data found')
+                } else{
+                    cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+                } 
+            })
+        })
+    })
+
+    it('Web datepickers', () => {
+
+        function selectDayFromCurrent(day){
+
+            let date = new Date()
+            date.setDate(date.getDate() + day)
+            let futureDay = date.getDate()
+            let futureMonth = date.toLocaleString('en-us', {month: 'short'})
+            let dateAssert = futureMonth+' '+futureDay+', '+date.getFullYear()
+
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then(dateAttribute => {
+                if(!dateAttribute.includes(futureMonth)){
+                    cy.get('[data-name="chevron-right"]').click()
+                    selectDayFromCurrent(day)
+                } else{
+                    cy.get('nb-calendar-day-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                }
+            })
+            return dateAssert
+        }
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then(input => {
+            cy.wrap(input).click()
+            let dateAssert = selectDayFromCurrent(5)
+    
+            cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+            cy.wrap(input).should('have.value', dateAssert)
+        })
+    })
+
+    it('Tooltip', () => {
+
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Tooltip').click()
+
+        cy.contains('nb-card', 'Colored Tooltips')
+            .contains('Default').click()
+        cy.get('nb-tooltip').should('contain', 'This is a tooltip')
+    })
+
+    it('Dialog box', () => {
+
+        cy.visit('/')
+        cy.contains('Tables & Data').click()
+        cy.contains('Smart Table').click()
+
+        //1
+        //cy.get('tbody tr').first().find('.nb-trash').click()
+        //cy.on('window:confirm', (confirm) => {
+        //    expect(confirm).to.equal('Are you sure you want to delete?')
+        //})
+
+        //2
+        //const stub = cy.stub()
+        //cy.on('window:confirm', stub)
+        //cy.get('tbody tr').first().find('.nb-trash').click().then(() => {
+        //    expect(stub.getCall(0)).to.be.calledWith('Are you sure you want to delete?')
+        //})
+
+        //3
+        cy.get('tbody tr').first().find('.nb-trash').click()
+        cy.on('window:confirm', () => false)
+    })
+
+
 })
